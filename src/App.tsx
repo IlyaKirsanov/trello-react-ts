fd492917acdf9372b37749454411045a79e46fa3
Ilya Kirsanov <ilya.kirsanov@dreamhost.com>
2021-05-26 20:16:43 +0300

    created basic app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { AddNewItem } from "./AddNewItem";
+import { AppContainer } from "./styles";
+import { Column } from "./Column";
+import { Card } from "./Card";
 
 function App() {
-  return <div></div>;
+  return (
+    <AppContainer>
+      <Column text="To Do">
+        <Card text="Generate app scaffold" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn TypeScript" />
+      </Column>
+      <Column text="Done">
+        <Card text="Begin to use static typing" />
+      </Column>
+      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
+    </AppContainer>
+  );
 }
 
 export default App;
